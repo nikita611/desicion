5ef3aeef19ebd0dbe28515b5db6540998343e384
nikita611 <nikita611@inbox.ru>
2016-10-21 18:03:20 +0300

    checksum

---
+++ checksum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int checksum(vector<int> vec)
+{
+	const int seed = 113;
+    const unsigned long long limit = 10000007;
+
+
+    unsigned long long sum = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        sum = (sum + v[i]) * 113;
+            sum %= limit;
+    }
+
+    return sum;
+}
+
+int main()
+{
+	return 0;
+}
